4f55b5da2ebc1ea31751c3de7e3e3ce867c648b2
git@hellemail.xyz <git@hellemail.xyz>
2023-02-22 22:54:12 +0000

    did it

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
-from flask import Flask, render_template,request
+from flask import Flask
 from flask_sock import Sock
+import socket
+
+app = Flask(__name__)
+sock = Sock(app)
+
+@app.route('/')
+def index():
+    return 'Use /protocol/ip/port to connect to a TCP or UDP over WebSockets.'
+
+@sock.route('/tcp/<ip>/<int:port>')
+def tcp(sock,ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    while True:
+        data = s.recv(1024)
+        if not data:
+            break
+        sock.send(data)
+    s.close()
+    sock.close()
+
+@sock.route('/udp/<ip>/<int:port>')
+def udp(sock,ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', port))
+    while True:
+        data, addr = s.recvfrom(1024)
+        if not data:
+            break
+        sock.send(data)
+    s.close()
+    sock.close()
+
+if __name__ == '__main__':
+    app.run()
